cd33c4601ff609ae0d086805a2e14df52ce5db3a
Vladislav Kuznetsov <starforce882@gmail.com>
2022-10-31 12:27:59 +0200

    Dotfiles are in convenient packs now

--- install-packets.sh
+++ install-packets.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 # prerequisites: you installed these packages:
-# base base-devel linux linux-firmware linux-lts linux-headers efibootmgr refind networkmanager
+# base base-devel linux linux-firmware linux-headers efibootmgr refind networkmanager
 # optional: wireguard-tools openresolv ueberzug
 
 sudo pacman -Syyu
 sudo pacman -S xorg xorg-init git ueberzug nvidia dwm picom alacritty feh nitrogen rofi neofetch htop ttf-jetbrains-mono ttf-font-awesome
 fc-cache -fv
-
-# install `yay`?
-# https://github.com/Jguer/yay
 
 # TODO: full installation script
